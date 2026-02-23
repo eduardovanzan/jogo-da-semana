a3e642a4dcaf4208be75b6efbe4d7303b1dadcd3
Eduardo Vanzan <eduardovanzan@gmail.com>
2026-02-23 00:31:44 -0300

    resultados

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -106,7 +106,7 @@ export default function Navbar() {
 
             <button
               onClick={handleLogout}
-              className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 transition-all duration-200"
+              className="cursor-pointer px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 transition-all duration-200"
             >
               Sair
             </button>
