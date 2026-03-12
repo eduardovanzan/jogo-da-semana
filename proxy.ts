8f883f23425dad83c03bef8e300522a5ba0f5023
Eduardo De Castro Vanzan De Almeida <49937@cbmerj.local>
2026-03-12 10:31:23 -0300

    proxy

--- proxy.ts
+++ proxy.ts
@@ -43,5 +43,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/escolhas/:path*", "/admin/:path*", "/votar/:path*", "/historico/:path*", "/ranking/:path*"],
+  matcher: ["/escolhas/:path*", "/admin/:path*", "/votar/:path*", "/historico/:path*", "/ranking/:path*", "/resultados/:path*"],
 };
